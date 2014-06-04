22e7735f25e188dfafd571b2b689cfaac8196b51
vandrada <vandrada1248@gmail.com>
2014-06-04 11:13:43 -0500

    Multiprocessed this too:)

--- parallel/chrome_split_batch.py
+++ parallel/chrome_split_batch.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
 """
 Simple script to batch process multiple bam files.
+
+Unfortunately, all files should be indexed and sorted already, only the minumum
+of flags (the --verbose flag) are passed to the main program.
 """
 import sys
+import subprocess
+from multiprocessing import Process
 
-to_process = sys.argv[1:]
+processes = []
 
-for bam in to_process:
-    subprocess.call(['chrome_split.py', '-f', bam])
+for bam in sys.argv[1:]:
+    p = Process(target=subprocess.call,
+                args=(['chrome_split', '-f', bam, '--verbose'], ))
+    p.daemon = True
+    processes.append(p)
+
+for p in processes:
+    p.start()
+
+for p in processes:
+    p.join()
