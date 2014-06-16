412b793bbbaf66de1d1e132b513c0eef0a9f5f79
vandrada <vandrada1248@gmail.com>
2014-06-16 10:54:30 -0500

    added build_samtools_args; added use of global variable

--- parallel/chromo_split.py
+++ parallel/chromo_split.py
@@ -34,7 +34,7 @@ try:
     from argparse import ArgumentParser
     from concurrent.futures import ThreadPoolExecutor
 except ImportError:
-    print "please install futures"
+    print "please install the needed Python modules"
     sys.exit()
 
 ################################################################################
@@ -75,7 +75,7 @@ def parse_header(samfile):
         print "> found sections: %s" % (', '.join(item for item in sections))
     return sections
 
-def build_varscan_args(arg_f, mpileup_f):
+def build_varscan_args(mpileup_f):
     """
     Parses a file containing the arguments for VarScan and returns a list for
     subprocess.open
@@ -83,13 +83,31 @@ def build_varscan_args(arg_f, mpileup_f):
 
     args = ["java", "-jar", varscan_location, action]
     if not with_pipe:
-        args.append(mpileup_f.name)
+        args.append(mpileup_f)
     LOCK.acquire()
-    for line in arg_f:
+    for line in varscan_conf:
         args.append(line.strip('\n'))
 
-    # rewind the file to the beginning for future calls to build_varscan_args
-    arg_f.seek(0)
+    LOCK.release()
+
+    return args
+
+def build_samtools_args(bam_f):
+    """
+    Parses a file containing the arguments for samtools mpileup and returns a
+    list for subprocess.open. Unfortunately, it's very similar to
+    build_varscan_args.
+    """
+    args = ["samtools", "mpileup"]
+    if not with_pipe:
+        args.append(bam_f)
+    else:
+        args.extend(["-", "-o", "-"])
+
+    LOCK.acquire()
+    for line in samtools_conf:
+        args.append(line.strip('\n'))
+
     LOCK.release()
 
     return args
@@ -130,7 +148,7 @@ def create_mpileup(region, bam_f):
     mpileup_f = open(os.path.join(mpileup_dir, region + ".mpileup"), "w+b")
     if args.verbose:
         print "> %s creating %s" % (strftime(t_format), mpileup_f.name)
-    subprocess.call(["samtools", "mpileup", bam_f.name], stdout=mpileup_f)
+    subprocess.call(build_samtools_args(bam_f.name), stdout=mpileup_f)
     if not args.keep_bam:
         os.remove(bam_f.name)
     if args.verbose:
@@ -145,14 +163,13 @@ def create_vcf(region, mpileup_f):
     vcf_f = open(os.path.join(vcf_dir, region + ".vcf"), "w+")
     if args.verbose:
         print "> %s creating vcf for %s" % (strftime(t_format), region)
-    subprocess.call(build_varscan_args(arg_f, mpileup_f), stdout=vcf_f)
+    subprocess.call(build_varscan_args(mpileup_f.name), stdout=vcf_f)
     if not args.keep_mpileup:
         os.remove(mpileup_f.name)
     if args.verbose:
         print "> %s FINISHED vcf file for %s" % (strftime(t_format), region)
 
     vcf_f.close()
-
 
 def run_with_pipe(region):
     """
@@ -165,11 +182,10 @@ def run_with_pipe(region):
         print "> %s starting region %s" % (strftime(t_format), region)
     bam = subprocess.Popen(["samtools", "view", "-b", bam_file.filename, region],
         stdout=subprocess.PIPE)
-    mpileup = subprocess.Popen(["samtools", "mpileup", "-", "-o", "-"],
-        stdin=bam.stdout, stdout=subprocess.PIPE)
+    mpileup = subprocess.Popen(build_samtools_args(""), stdin=bam.stdout,
+        stdout=subprocess.PIPE)
     vcf_f = open(os.path.join(vcf_dir, region + ".vcf"), "w+")
-    subprocess.call(build_varscan_args(arg_f, ""), stdin=mpileup.stdout,
-        stdout=vcf_f)
+    subprocess.call(build_varscan_args(""), stdin=mpileup.stdout, stdout=vcf_f)
 
     # close the pipes
     bam.stdout.close()
@@ -205,7 +221,7 @@ def run_processes(infile):
         print "> parsing header sections"
     with ThreadPoolExecutor(max_workers=args.n_procs) as executor:
         for region in parse_header(infile):
-            if not args.with_pipe:
+            if not with_pipe:
                 executor.submit(run, region)
             else:
                 executor.submit(run_with_pipe, region)
@@ -220,6 +236,11 @@ if __name__ == '__main__':
         help="the action for VarScan to run")
     parser.add_argument("varscan_location",
         help="absolute path to the VarScan jar file")
+    # conf files
+    parser.add_argument("--varscan-conf", dest="varscan_conf", default=None,
+        help="the location of varscan.conf (defaults to the working directory)")
+    parser.add_argument("--samtools-conf", dest="samtools_conf", default=None,
+        help="the location of samtools.conf (defaults to the working directory")
     # options
     parser.add_argument("--with-pipe", action="store_true", dest="with_pipe",
         help="instead of writing to disk, the commands are piped")
@@ -231,8 +252,6 @@ if __name__ == '__main__':
         dest="keep_mpileup", help="keeps the intermediate mpileup files")
     parser.add_argument("--keep-all", action="store_true", dest="keep_all",
         help="keeps bam and mpileup files")
-    parser.add_argument("--varscan-conf", dest="varscan_conf", default=None,
-        help="the location of varscan.conf (defaults to the working directory)")
     parser.add_argument("--sort", action="store_true", dest="sort",
         help="use if the file needs to be sorted (implies --index)")
     parser.add_argument("--index", action="store_true", dest="index",
@@ -242,16 +261,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     LOCK = Lock()
+
     bam_file = pysam.Samfile(str(args.infile), "rb")
 
     # global variables just to save some typing
     action = args.action
     with_pipe = args.with_pipe
-    arg_f = args.varscan_conf
+    varscan_conf = args.varscan_conf
+    samtools_conf = args.samtools_conf
 
     # test to see if a default varscan.conf should be used
-    if arg_f == None:
-        arg_f = open("varscan.conf", "r")
+    if varscan_conf == None:
+        varscan_conf = open("varscan.conf", "r").readlines()
+    if samtools_conf == None:
+        samtools_conf = open("samtools.conf", "r").readlines()
     # test for PERL5LIB before any work is done
     #try:
     #    os.environ['PERL5LIB']
