204ec38e501e8364656010e88cbbf3b2fa42132b
vandrada <vandrada1248@gmail.com>
2014-06-24 14:42:53 -0500

    added install function

--- chromoprocessor/install.py
+++ chromoprocessor/install.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
+"""
+Installs the dependencies of chromoprocessor
+"""
+
 import sys
 import urllib
 import os
 import tarfile
+import shutil
 import subprocess
-from os.path import expanduser
-
-argparse =\
-    "http://pypi.python.org/packages/source/a/argparse/argparse-1.2.1.tar.gz"
-
-home = expanduser('~')
-os.chdir(home)
 
 def dir_name(url):
     """
@@ -19,23 +17,36 @@ def dir_name(url):
     """
     return '.'.join(url.split('/')[-1].split('.')[:-2])
 
-try:
-    import blah
-except ImportError:
-    argparse_dir = dir_name(argparse)
-    print "installing argparse to ~/%s" % (argparse_dir)
-    urllib.urlretrieve(argparse, filename="argparse.tar.gz")
+def install(lib, url):
+    os.chdir(HOME)
+    lib_dir = dir_name(lib)
+    print "installing %s to ~/%s" (lib, lib_dir)
+    urllib.urlretrieve(argparse, file=lib + ".tar.gz")
     # extract
-    argparse_tar = tarfile.open("argparse.tar.gz")
-    argparse_tar.extractall()
-    argparse_tar.close()
-    os.remove(argparse_tar.name)
+    lib_tar = tarfile.open(lib + ".tar.gz")
+    lib_tar.extractall()
+    lib_tar.close()
+    os.remove(lib_tar.name)
     # install
-    os.chdir(argparse_dir)
+    os.chdir(lib_dir)
     subprocess.call(['python', 'setup.py', 'install', '--user'])
+    os.chdir(HOME)
+    shutil.rmtree(lib_dir)
 
-try:
-    import concurrent.futures
-except ImportError:
-    print "installing concurrent"
-    # install concurrent
+def main():
+    # URLs
+    argparse_url =\
+        "http://pypi.python.org/packages/source/a/argparse/argparse-1.2.1.tar.gz"
+    futures_url =\
+        "http://pypi.python.org/packages/source/f/futures/futures-2.1.6.tar.gz"
+    try:
+        import argparse
+    except ImportError:
+        install("argparse", argparse_url)
+    try:
+        import concurrent.futures
+    except ImportError:
+        install("concurrent", futures_url)
+
+if __name__ == '__main__':
+    HOME = os.path.expanduser('~')
