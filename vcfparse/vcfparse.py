4de2787f34f6e8b17066e52d88b43c0545a94136
vandrada <vandrada1248@gmail.com>
2014-06-26 14:23:08 -0500

    add pretty option

--- vcfparse/vcfparse.py
+++ vcfparse/vcfparse.py
@@ -21,11 +21,13 @@ def parse(vcf_file, fields):
     samples = vcf_file.samples
 
     # header
-    header(samples, fields)
+    if args.pretty:
+        header(samples, fields)
 
     # begin parsing (and printing)
     for record in vcf_file:
-        print "{0:4}:{1:10}".format(record.CHROM, str(record.POS)),
+        if args.pretty:
+            print "{0:4}:{1:10}".format(record.CHROM, str(record.POS)),
         for sample in record:
             if sample.called:
                 for field in fields:
@@ -37,10 +39,13 @@ def parse(vcf_file, fields):
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
     argparse.add_argument('vcf_file', help="the vcf file to parse")
-    argparse.add_argument('fields', nargs='+', type=str,
+    argparse.add_argument('--fields', nargs='+', type=str, default=[],
         help="the sections to parse from the vcf file")
-
+    argparse.add_argument('--pretty', action="store_true",
+        help="print a header and chromosome information")
     args = argparse.parse_args()
+
     SEP = '\t\t'
     vcf_file = vcf.Reader(open(args.vcf_file, 'r'))
+
     parse(vcf_file, [field.upper() for field in args.fields])
