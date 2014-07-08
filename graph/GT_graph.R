8c3bb1cc4fbcb1b8ef857ab00636f948d3e94612
vandrada <vandrada1248@gmail.com>
2014-07-08 10:45:21 -0500

    actually call the make.plots

--- graph/GT_graph.R
+++ graph/GT_graph.R
@@ -10,7 +10,7 @@ plot.sample <- function(samp, par) {
     geom_bar() + 
     labs(x="Genotype", y="Count", title=par) +
     stat_bin(geom="text", aes(label=..count..), vjust=0.10, hjust=0.0,
-                            size=3, colour="red") + 
+             size=3, colour="red") + 
     theme(legend.position="none", text=element_text(size=6))
 }
 
@@ -33,20 +33,7 @@ headers <- as.list(read.table("~/Desktop/run2/processed.txt", sep="\n"))
 colnames(gt) <-
   c("Chromosome", "Position", readLines("~/Desktop/run2/processed.txt"))
 
-# # Create data frames for the individual samples
-# # olap data
-# c10.olap <- gt[, grep("C10_olap", names(gt), value=TRUE)]
-# c258.olap <- gt[, grep("C258_olap", names(gt), value=TRUE)]
-# ews.olap <- gt[, grep("EWS_olap", names(gt), value=TRUE)]
-# imr90.olap <- gt[, grep("IMR90_olap", names(gt), value=TRUE)]
-# t32.olap <- gt[, grep("T32_olap", names(gt), value=TRUE)]
-# 
-# # et data
-# c10.et <- gt[, grep("C10_et", names(gt), value=TRUE)]
-# c258.et <- gt[, grep("C258_et", names(gt), value=TRUE)]
-# ews.et <- gt[, grep("EWS_et", names(gt), value=TRUE)]
-# imr90.et <- gt[, grep("IMR90_et", names(gt), value=TRUE)]
-# t32.et <- gt[, grep("T32_et", names(gt), value=TRUE)]
+# create the data for each cell line
 c10 <- gt[, grep("C10", names(gt), value=T)]
 c258 <- gt[, grep("C258", names(gt), value=T)]
 ews <- gt[, grep("EWS", names(gt), value=T)]
@@ -55,3 +42,10 @@ t32 <- gt[, grep("T32", names(gt), value=T)]
 
 # remove the initial data since it can be pretty big
 rm(gt)
+
+# graph the data
+make.plots(c10, "C10")
+make.plots(c258, "C258")
+make.plots(ews, "EWS")
+make.plots(imr, "IMR90")
+make.plots(t32, "T32")
