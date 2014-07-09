7f5cd3aeb304aae11d474339bda3109c87524f51
vandrada <vandrada1248@gmail.com>
2014-07-09 14:34:58 -0500

    graphs according to cell line

--- graph/FREQ_graph.R
+++ graph/FREQ_graph.R
@@ -1,31 +1,57 @@
 #!/usr/bin/env Rscript
 
 library(ggplot2)
+library(gridExtra)
+library(gtools)
 
-plot.sample <- function(samp, col.n) {
-  temp <- as.data.frame(cut(samp[[col.n]],
-                        breaks=c(0.01, 0.02, 0.1, 0.4, 1.0)))
+freq.file <- "~/Desktop/run2/FREQ2.txt"
+file.names <- "~/Desktop/run2/processed.txt"
+
+# plots a single sample
+plot.sample <- function(samp, par) {
+  temp <- as.data.frame(cut(samp[[grep(par, colnames(samp), value=TRUE)]],
+                        breaks=c(0.0, 0.01, 0.02, 0.1, 0.4, 1.0)))
   colnames(temp) <- c("sample")
   ggplot(na.omit(temp), aes_string(x="sample")) +
-    geom_bar() + 
-    labs(x="Frequency", y="Count", title=colnames(samp)[col.n]) +
-    #stat_bin(geom="text", aes(label=..count..), vjust=0.10, hjust=0.0,
-    #         size=3, colour="red") + 
-    theme(legend.position="none", text=element_text(size=10))
+    geom_bar() +
+    labs(x="Frequency", y="Count", title=par) +
+    theme(legend.position="none", text=element_text(size=6),
+          axis.text.x=element_text(angle=90))
 }
 
-freq <- read.table("~/Desktop/run2/FREQ2.txt", header=TRUE, fill=TRUE,
-                   na.strings='./.')
-colnames(freq) <-
-  c("Chromosome", "Position", readLines("~/Desktop/run2/processed.txt"))
+# plots multiple samples
+make.plots <- function(df, name) {
+  # sort colnames
+  gs <- lapply(mixedsort(colnames(df)), FUN=function(x) plot.sample(df, x))
+  n <- length(gs)
+  nCol <- floor(sqrt(n))
+  pdf(paste(name, "pdf", sep="."), width=8, height=11)
+  do.call("grid.arrange", c(gs))
+  dev.off()
+}
+
+freq <- read.table(freq.file, header=TRUE, fill=TRUE, na.strings='./.')
+colnames(freq) <- c("Chromosome", "Position", readLines(file.names))
 
 # turn percentages to real numbers
 freq[3:length(freq)] <- sapply(freq[3:length(freq)],
                                function(x) as.numeric(sub("%", "", x)) / 100)
 freq[3:length(freq)] <- as.data.frame(sapply(freq[3:length(freq)], as.numeric))
 
-#for each sample
-temp <- as.data.frame(cut(freq[[3]], breaks=(c(0.01, 0.02, 0.1, 0.4, 1.0))))
-colnames(temp) <- c("sample")
-ggplot(na.omit(temp), aes(x=sample)) + geom_bar() +
-  labs(x="Frequency", y="Count")
+# plot...
+# create the data for each cell line
+c10 <- freq[, grep("C10", names(freq), value=T)]
+c258 <- freq[, grep("C258", names(freq), value=T)]
+ews <- freq[, grep("EWS", names(freq), value=T)]
+imr <- freq[, grep("IMR90", names(freq), value=T)]
+t32 <- freq[, grep("T32", names(freq), value=T)]
+
+# remove the initial data since it can be pretty big
+rm(freq)
+
+# graph the data
+make.plots(c10, "C10")
+make.plots(c258, "C258")
+make.plots(ews, "EWS")
+make.plots(imr, "IMR90")
+make.plots(t32, "T32")
