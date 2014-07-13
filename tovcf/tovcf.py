e46b44ec6db29290c0a01bd0970bdafdf39fb8d0
vandrada <vandrada1248@gmail.com>
2014-07-13 01:24:49 -0500

    program now takes an xls(s) file; changed dict keys to uppercase

--- tovcf/tovcf.py
+++ tovcf/tovcf.py
@@ -2,8 +2,27 @@
 
 import csv
 import sys
+import os
 import time
 import argparse
+import xlrd
+
+def create_csv(xls_file):
+    """
+    Creates a csv file from an Excel file.
+    :param xls_file: the name of the Excel file
+    :return: the name of the csv file
+    ** Ripped off from Boud at StackOverflow **
+    """
+    csv_file = os.path.splitext(xls_file)[0] + ".csv"
+    with xlrd.open_workbook(xls_file) as xls:
+        sheet = xls.sheet_by_index(0)
+        with open(csv_file, "wb") as f:
+            c = csv.writer(f)
+            for row in range(sheet.nrows):
+                c.writerow(sheet.row_values(row))
+
+    return csv_file
 
 def header():
     """
@@ -55,21 +74,21 @@ def write_fields(in_file):
     get = lambda field: '.' if row[FIELDS[field]] == 'NULL'\
                             else row[FIELDS[field]]
 
-    create_record = lambda: ":".join([genotype(get("ref"), get("var")),
-                                      get("cov"), get("qs"), get("trans"),
-                                      get("cm"), get("pm"), get("oas"),
-                                      get("eas")])
+    create_record = lambda: ":".join([genotype(get("REF"), get("VAR")),
+                                      get("COV"), get("QS"), get("TRANS"),
+                                      get("CM"), get("PM"), get("OAS"),
+                                      get("EAS")])
 
     line = ""
     record = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n"
     for row in in_file:
-        line = record.format(get("chr"), get("co"), get("db"), get("ref"),
-                             get("var"), ".", ".", ".", FORMAT, create_record())
+        line = record.format(get("CHR"), get("CO"), get("DB"), get("REF"),
+                             get("VAR"), ".", ".", ".", FORMAT, create_record())
         out.write(line)
 
 if __name__ == '__main__':
     argparse = argparse.ArgumentParser()
-    argparse.add_argument("file", help="the csv file to process")
+    argparse.add_argument("file", help="the xls(x) file to process")
     argparse.add_argument("--out", dest="outfile", default=None,
         help="the output file")
     args = argparse.parse_args()
@@ -77,11 +96,12 @@ if __name__ == '__main__':
     # Constants
     FORMAT = "GT:COV:QS:TS:CM:PM:OAS:EAS"
     # fields from the csv
-    cols = ["chr", "co", "ref", "var", "cov", "qs", "zyg", "gene", "trans",
-            "cm", "pm", "db", "oas", "eas"]
+    cols = ["CHR", "CO", "REF", "VAR", "COV", "QS", "ZYG", "GENE", "TRANS",
+            "CM", "PM", "DB", "OAS", "EAS"]
     FIELDS = dict(zip(cols, range(len(cols))))
 
-    in_file = csv.reader(open(args.file, "r"))
+    csv_file = create_csv(args.file)
+    in_file = csv.reader(open(csv_file, "r"))
     in_file.next()      # read the first line and discard
     out = sys.stdout
 
@@ -90,3 +110,5 @@ if __name__ == '__main__':
 
     out.write(header())
     write_fields(in_file)
+
+    os.remove(csv_file)
