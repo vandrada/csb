87d12cd9d208cabb28a71d920d92ce018423be34
vandrada <vandrada1248@gmail.com>
2014-08-04 15:30:43 -0500

    added GPL

--- graph/geno.R
+++ graph/geno.R
@@ -1,8 +1,24 @@
-# improved
+#!/usr/bin/env Rscript
+
+# Copyright (C) 2014 Andrada, Vicente
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
 library("ggplot2")
 library("gtools")
 
-melted <- read.table("~/Desktop/test/GT.txt", fill=TRUE, header=T, 
+melted <- read.table("~/Desktop/test/GT.txt", fill=TRUE, header=T,
                      na.strings=c('./.', '0/0'))
 
 # set up the data frame
@@ -17,4 +33,4 @@ ggplot(data=na.omit(t32.olap), aes(x=Value)) + geom_bar() +
   facet_grid(. ~ Sample) + labs(x="Genotype", y="Count",
                                 title="T32 Olaparib Genotype") +
   theme(text=element_text(size=8))
-#ggsave(file="t32_gt.pdf", width=11, height=4.25)
+ggsave(file="t32_gt.pdf", width=11, height=4.25)
--- graph/gt_line.R
+++ graph/gt_line.R
@@ -1,9 +1,25 @@
-# improved
+#!/usr/bin/env Rscript
+
+# Copyright (C) 2014 Andrada, Vicente
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
 library("ggplot2")
 library("gtools")
 library("plyr")
 
-melted <- read.table("~/Desktop/test/GT.txt", fill=TRUE, header=T, 
+melted <- read.table("~/Desktop/test/GT.txt", fill=TRUE, header=T,
                      na.strings='./.')
 
 # set up the data frame
@@ -19,4 +35,4 @@ awesome <- count(na.omit(t32.olap), c("Sample", "Value"))
 ggplot(na.omit(awesome), aes(x=Sample, y=freq, group=Value, color=Value)) +
   geom_line() + labs(x="Hour", y="Count", title="T32 Olaparib Genotype") +
   scale_x_discrete(labels=c("0", "6", "12", "18", "24"))
-#ggsave(file="t32_gt.pdf", width=11, height=4.25)
+ggsave(file="t32_gt.pdf", width=11, height=4.25)
